ff930776139a5cd61984606ff59f5645b3661da9
ingrid <fermingrid@gmail.com>
2017-05-18 16:03:44 +0200

    mockito done

--- dagger2/src/main/java/musicians/Singer.java
+++ dagger2/src/main/java/musicians/Singer.java
@@ -8,7 +8,7 @@ public class Singer {
 
     private long decibel;
     private String ROCK = "I'm Axl Rose\n";
-    private String POP = "I'm Madonnan\n";
+    private String POP = "I'm Madonna\n";
 
     public void setDecibel(long decibel) {
         this.decibel = decibel;
